041a47cf63066b55829b28a85f95fc9257fbaa38
Krzysztof Urbaniec <urbaniec.krzysztof@interia.pl>
2023-12-28 21:47:30 +0100

    Created sketch of main.py file

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from database import db_manager
+from scraper import scraper
+
+def main():
+    # Connect to MySQL server
+    # Create heroes database if does not exist
+    # Create tables
+    # Scrap data
+    # Insert data into the tables
+    pass
+
+if __name__ == '__main__':
+    main()
