1c49a757a70b3d98e9052ab184097201aee5d713
Krzysztof Urbaniec <urbaniec.krzysztof@interia.pl>
2024-01-02 20:20:15 +0100

    Modified function "get_creature_data()"
    Now it saves scraped data to a dictionary and returns a list filled with them

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -33,31 +33,31 @@ def get_creature_data(creature_soup: BeautifulSoup) -> list:
         creature_soup: BeautifulSoup object containing scraped html page with creatures table.
 
     Returns:
-        Data prepared for an insertion into MySQL database in format: [(Value1,Value2,...,ValueN),(Value1,Value2,...,ValueN),...] '''
+        Data prepared for an insertion into MySQL database in format: [dict(values),dict(values),...] '''
 
     creatures = []
-
     table = creature_soup.find_all('tr')[2:]
     for row in table:
+        creature_properties = dict()
         unit_properties = row.find_all('td')
-        unit_name = unit_properties[0].text.strip()
-        town = unit_properties[1].find('span').get('title').strip()
-        level =  unit_properties[2].text.strip()
-        attack = unit_properties[3].text.strip()
-        defense = unit_properties[4].text.strip()
-        min_dmg = unit_properties[5].text.strip()
-        max_dmg = unit_properties[6].text.strip()
-        health = unit_properties[7].text.strip()
-        speed = unit_properties[8].text.strip()
-        growth = unit_properties[9].text.strip()
-        gold = unit_properties[11].text.strip()
-        second_resource = unit_properties[12].text.replace(',','').strip()
-        special_abilities = unit_properties[13].text.strip()
+        creature_properties['unit_name'] = unit_properties[0].text.strip()
+        creature_properties['town'] = unit_properties[1].find('span').get('title').strip()
+        creature_properties['level'] =  unit_properties[2].text.strip()
+        creature_properties['attack'] = unit_properties[3].text.strip()
+        creature_properties['defense'] = unit_properties[4].text.strip()
+        creature_properties['min_dmg'] = unit_properties[5].text.strip()
+        creature_properties['max_dmg'] = unit_properties[6].text.strip()
+        creature_properties['health'] = unit_properties[7].text.strip()
+        creature_properties['speed'] = unit_properties[8].text.strip()
+        creature_properties['growth'] = unit_properties[9].text.strip()
+        creature_properties['gold'] = unit_properties[11].text.strip()
+        creature_properties['second_resource'] = unit_properties[12].text.replace(',','').strip()
+        creature_properties['special_abilities'] = unit_properties[13].text.strip()
             
-        if second_resource != '':
-            second_resource += ' ' + unit_properties[12].find('a').get('title').strip()
+        if creature_properties['second_resource'] != '':
+            creature_properties['second_resource'] += ' ' + unit_properties[12].find('a').get('title').strip()
 
-        creatures.append((unit_name, town, level, attack, defense, min_dmg, max_dmg, health, speed, growth, gold, second_resource, special_abilities))
+        creatures.append(creature_properties)
 
     return creatures
 
