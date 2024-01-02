01e44bf63ceb14d137833bd004d53cf943e2c980
Krzysztof Urbaniec <urbaniec.krzysztof@interia.pl>
2024-01-02 21:01:15 +0100

    Added documentation

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -7,7 +7,13 @@ import mysql.connector
 load_dotenv()
 
 def create_mysql_connection(func):
-    '''Create connection to MySQL server.'''
+    '''Create a connection to MySQL server. 
+    
+    It's a wrapper function, which allows to modulate the behavior of other functions interacting with databases.
+
+    Args:
+        func: function, upon which the wrapper is intended to act.
+    '''
 
     @functools.wraps(func)
     def wrapper(host,user,password,database=None,*args,**kwargs):
@@ -22,20 +28,29 @@ def create_mysql_connection(func):
     return wrapper
 
 @create_mysql_connection
-def create_table(connection,table_query):
-    '''Create table in an existing database.'''
+def create_table(connection: mysql.connector.connection_cext.CMySQLConnection, table_query: 'str') -> None:
+    '''Create table in an existing database.
+    
+    Args:
+        connection: mysql.connector's object representing a connection to a database.
+        table_query: query responsible for table creation.
+    '''
     with connection.cursor() as cursor:
         try:
             cursor.execute(table_query)
         except mysql.connector.Error as err:
             print(err.msg)
     print('Table created.')
-        
 
-# Function that performs an SQL insertion operation
 @create_mysql_connection
-def insert_into_table(connection, table_name, data):
-    '''Insert data into an existing table.'''
+def insert_into_table(connection: mysql.connector.connection_cext.CMySQLConnection, table_name: str, data: list) -> None:
+    '''Insert data into an existing table.
+    
+    Args:
+        connection: mysql.connector's object representing a connection to a database.
+        table_name: name of the table to insert data into.
+        data: a list of dictionaries containing data to insert into the table.
+    '''
     with connection.cursor() as cursor:
         columns_list = ', '.join(data[0].keys())
         values_list = ', '.join(list(map(lambda x: '%(' + str(x) + ')s',data[0].keys())))
@@ -43,24 +58,3 @@ def insert_into_table(connection, table_name, data):
         insert_query = f"INSERT INTO {table_name} ({columns_list}) VALUES ({values_list})"
         cursor.executemany(insert_query, data)
         connection.commit()
-
-db_params = {'user': os.getenv('MYSQL_USER'),
-             'host': os.getenv('MYSQL_HOST'),
-             'password': os.getenv('MYSQL_PASSWORD'),
-             'database': 'Heroes3DB'}
-
-# Insert data into the table using the decorated function
-# insert_into_table(db_params['host'], db_params['user'], db_params['password'], db_params['database'], table, data_to_insert)
-
-from src.scraper import scraper
-creature_soup = scraper.fetch_and_parse('https://heroes.thelazy.net/index.php/List_of_creatures')
-creature_data = scraper.get_creature_data(creature_soup)
-
-table_query = (
-    "CREATE TABLE IF NOT EXISTS `test_table_creatures` ("
-    " `unit_name` VARCHAR(100), `town` VARCHAR(20), `level` VARCHAR(5), `attack` INT, `defense` INT, `min_dmg` INT, `max_dmg` INT, `health` INT, `speed` INT,"
-    " `growth` INT, `gold` INT, `second_resource` VARCHAR(50), `special_abilities` VARCHAR(255)"
-    ")")
-
-create_table(db_params['host'], db_params['user'], db_params['password'], db_params['database'],table_query=table_query)
-insert_into_table(db_params['host'], db_params['user'], db_params['password'], db_params['database'],table_name='test_table_creatures',data=creature_data)
