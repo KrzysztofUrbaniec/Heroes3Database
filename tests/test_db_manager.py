c0acc5e08f04d289c2d1d3fad9d7fcef2fbcacb4
Krzysztof Urbaniec <urbaniec.krzysztof@interia.pl>
2024-01-09 11:00:03 +0100

    Finished tests for function generate_insert_query()

--- tests/test_db_manager.py
+++ tests/test_db_manager.py
@@ -1,28 +1,8 @@
 import unittest
-from unittest.mock import MagicMock, patch
 
 import src.database.db_manager as dbm
 
 class TestDBManager(unittest.TestCase):
-
-    # Test connection
-    # def test_insert_into_table(self):
-    #     mock_connection = MagicMock()
-
-    #     # Your test data
-    #     test_table_name = 'test_table'
-    #     test_data = [
-    #         {'id': 1, 'name': 'Alice'},
-    #         {'id': 2, 'name': 'Bob'},
-    #         {'id': 3, 'name': 'Charlie'}
-    #     ]
-
-    #     with patch('mysql.connector.connect') as mock_connect:
-    #         mock_connect.return_value = mock_connection
-
-    #         dbm.insert_into_table(test_table_name, test_data)
-
-    #     mock_connection.cursor.assert_called_once() 
 
     def test_generate_insert_query_normal(self):
         test_table_name = 'test_table'
@@ -34,14 +14,14 @@ class TestDBManager(unittest.TestCase):
 
         expected_query = "INSERT INTO test_table (id, name) VALUES (%(id)s, %(name)s)"
         generated_query = dbm.generate_insert_query(test_data,test_table_name)
-        self.assertEqual(expected_query,generated_query)
+        self.assertEqual(expected_query, generated_query)
 
     def test_generate_insert_query_empty_list(self):
         test_table_name = 'test_table'
         test_data = []
 
         with self.assertRaises(ValueError):
-            dbm.generate_insert_query(test_data,test_table_name)
+            dbm.generate_insert_query(test_data, test_table_name)
 
     def test_generate_insert_query_with_bad_data_input(self):
         test_table_name = 'test_table'
@@ -77,8 +57,8 @@ class TestDBManager(unittest.TestCase):
         test_data = [{'id': 3, 'name': 'Charlie'}]
 
         expected_query = "INSERT INTO test_table (id, name) VALUES (%(id)s, %(name)s)"
-        generated_query = dbm.generate_insert_query(test_data,test_table_name)
-        self.assertEqual(expected_query,generated_query)
+        generated_query = dbm.generate_insert_query(test_data, test_table_name)
+        self.assertEqual(expected_query, generated_query)
 
     def test_generate_insert_query_nonequal_number_of_keys(self):
         test_table_name = 'test_table'
@@ -89,7 +69,7 @@ class TestDBManager(unittest.TestCase):
         ]
 
         with self.assertRaises(ValueError):
-            dbm.generate_insert_query(test_data,test_table_name)
+            dbm.generate_insert_query(test_data, test_table_name)
 
     def test_generate_insert_query_mixed_keys(self):
         test_table_name = 'test_table'
@@ -100,7 +80,7 @@ class TestDBManager(unittest.TestCase):
         ]
 
         with self.assertRaises(ValueError):
-            dbm.generate_insert_query(test_data,test_table_name)
+            dbm.generate_insert_query(test_data, test_table_name)
 
 if __name__ == '__main__':
     unittest.main()
